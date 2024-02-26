ebbeb2a3016a5d6c8e58d12dea5971f65e6e3361
harry <harrydlr@gmail.com>
2024-02-27 00:36:05 +0100

    Code Refactoring

--- config/config.py
+++ config/config.py
@@ -4,6 +4,6 @@ import os
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 # Define paths to specific directories relative to the project root
-NETGEN_DIR = os.path.join(PROJECT_ROOT, 'netget')
+NETGEN_DIR = os.path.join(PROJECT_ROOT, 'netgen')
 TESTS_DIR = os.path.join(PROJECT_ROOT, 'tests')
 CONFIG_DIR = os.path.join(PROJECT_ROOT, 'config')
